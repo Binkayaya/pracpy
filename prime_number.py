4e8fb52351e03cd6cef66c6baa18af8bbe4e343e
stanage <binkayaya@gmail.com>
2025-03-24 15:07:20 +0100

    Replaced the fizzbuzz file without the .py extension with the file that has the extension. And also modified the prime_number.py code to not use the line that removed 1 from the list of prime numbers

--- prime_number.py
+++ prime_number.py
@@ -1,20 +1,18 @@
-# in this excercise, we will print out sll prime numbers between 1 and 100
-
+# in this excercise, we will print out all prime numbers between 1 and 100
+ 
 # i think that every number between 1 and 100 that isnt divisible by any number between 1 and 9 is a prime number.
 # So we get a list of numbers between 1 and 9, and we use a loop to go through to divide the targets which
 # will also be a loop from 1 to hundred.
 
 mod_list = []
-one = [1]
-target = range(100)
+target = range(2, 101)
 check = [2,3,4,5,6,7,8,9]
 for number in target:
     for divisor in check:
         if number % divisor == 0 and number != divisor:
             mod_list.append(number)
 
-hush = list(set(target) - set(mod_list) )
-prime = sorted(list(set(hush) - set(one)))
+prime = list(set(target) - set(mod_list) )
 print(f'there are {len(prime)} prime numbers between 1 and 100. They are given below: ')
 print(prime)
 
