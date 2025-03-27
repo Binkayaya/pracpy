b3f3ee456ee2762d2b237472bb566069d79575bc
Binkayaya <binkayaya@gmail.com>
2025-03-27 22:39:39 +0100

    Partially solved the zero problem.

--- reverse_number.py
+++ reverse_number.py
@@ -1,10 +1,15 @@
 #reverse number
 
-number = 37892
+number = 32015
 newlist = []
-while number % 10 != 0:
-    newlist.append(number % 10)
-    number = number // 10
+while number > 0:
+    if number % 10 == 0:
+        newlist.append(0)
+        number = number // 10
+    elif number % 10 != 0:
+        while number % 10 != 0:
+            newlist.append(number % 10)
+            number = number // 10
     
 listing = []
 
