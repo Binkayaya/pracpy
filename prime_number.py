9639b0c5a5b03c5d667a6d05b3734053ebf73cab
Binkayaya <binkayaya@gmail.com>
2025-04-10 21:59:26 +0100

    Corrected the prime number exercise to use square root to efficiently reduce the number og divisors to try on, and also made the code a function to allow for easy reusability

--- prime_number.py
+++ prime_number.py
@@ -1,23 +1,18 @@
-# in this excercise, we will print out all prime numbers between 1 and 100
- 
-# i think that every number between 1 and 100 that isnt divisible by any number between 1 and 9 is a prime number.
-# So we get a list of numbers between 1 and 9, and we use a loop to go through to divide the targets which
-# will also be a loop from 1 to hundred.
-
-mod_list = []
-target = range(2, 101)
-check = [2,3,4,5,6,7,8,9]
-for number in target:
-    for divisor in check:
-        if number % divisor == 0 and number != divisor:
-            mod_list.append(number)
-
-prime = list(set(target) - set(mod_list) )
-print(f'there are {len(prime)} prime numbers between 1 and 100. They are given below: ')
-print(prime)
-
-       
-
-
+def check_prime(n):
+    primes = []
     
-    
+    for num in range(2,n+1):
+        is_prime = True
+        for divisor in range(2, int((num)**0.5)+1):
+            if num % divisor == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(num)
+    return primes
+        
+n = int(input('Input a number: '))
+primes = check_prime(n)
+print(primes)
+print(f'There are {len(primes)} prime numbers between lower than {n}')
+
