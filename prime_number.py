5505242022856904fc7fc429d4db29728e3b1670
Binkayaya <binkayaya@gmail.com>
2025-04-11 20:24:50 +0100

    Added the Sieve of Erathosthenes Algorithm. It is an interesting approach, I must say. Basically, it removes all multiples of prime numbers starting from the first, which is 2. Really cool!

--- prime_number.py
+++ prime_number.py
@@ -14,5 +14,22 @@ def check_prime(n):
 n = int(input('Input a number: '))
 primes = check_prime(n)
 print(primes)
-print(f'There are {len(primes)} prime numbers between lower than {n}')
+print(f'There are {len(primes)} prime numbers lower than {n}')
 
+# Using the Sieve of Eratosthenes Algorithm. Just uncomment it and run.
+'''def sieve(n):
+    primes = [True] * (n+1)
+    primes[0], primes[1] = False, False
+    
+    for i in range(2, int(n**0.5)+1):
+        if primes[i]:
+            for multiples in range(i*i, n+1, i):
+                primes[multiples] = False
+
+    return [num for num, is_prime in enumerate(primes) if is_prime]
+
+n = int(input('Input a number: '))
+primes = sieve(n)
+print(primes)
+print(f'There are {len(primes)} prime numbers lower than {n}')
+'''
